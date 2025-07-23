17769ca3ac18a7ae17a7681088ce038629a93926
ankitalodha05 <76703320+ankitalodha05@users.noreply.github.com>
2025-07-23 22:12:01 +0530

    Create backend_setup_script.sh

---
+++ Terraform/backend_setup_script.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+sudo apt update -y
+sudo apt install -y nodejs npm
+git clone https://github.com/ankitalodha05/TravelMemory.git
+cd TravelMemory
+echo MONGO_URI='mongodb+srv://ankitalodha05:HXbLCIGpRkRg9gNn@cluster10.as960.mongodb.net/ankitalodha" > .env
+npm install
+nohup node index.js > output.log 2>&1 &
