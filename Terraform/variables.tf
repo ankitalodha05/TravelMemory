f2d37a77e3d0ffd78fad718a1b4089bcbfc7f4af
ankitalodha05 <76703320+ankitalodha05@users.noreply.github.com>
2025-07-23 22:04:35 +0530

    Create variables.tf

---
+++ Terraform/variables.tf
@@ -0,0 +1,11 @@
+variable "instance_type" {
+  default = "t2.micro"
+}
+
+variable "key_name" {
+  default = "my-key"
+}
+
+variable "ami_id" {
+  default = "ami-12345678" # Replace with valid AMI ID
+}
